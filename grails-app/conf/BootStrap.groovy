fa5443ba9b1d8c05fcdb905980099cbeefccd165
Sean Cahill <projects@nmm.ie>
2014-03-21 15:12:12 +0000

    fixed gsp error

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -17,7 +17,7 @@ class BootStrap {
      
                         def adminUser = User.findByUsername('admin') ?: new User(
                                         username: 'admin',
-                                        password: 'admin',
+                                        password: '30lospinos',
                                         enabled: true).save(failOnError: true)
                                     
                         def userUser = User.findByUsername('lillylangtree') ?: new User(
@@ -65,6 +65,42 @@ class BootStrap {
         } else 
         if (Environment.current == Environment.PRODUCTION) {
             // insert Production environment specific code here
+            def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
+                        def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+     
+                        def adminUser = User.findByUsername('admin') ?: new User(
+                                        username: 'admin',
+                                        password: '30lospinos',
+                                        enabled: true).save(failOnError: true)
+                                    
+                        def userUser = User.findByUsername('lillylangtree') ?: new User(
+                                        username: 'lillylangtree',
+                                        password: '30lospinos',
+                                        enabled: true).save(failOnError: true)
+
+                        if (!adminUser.authorities.contains(adminRole)) {
+                                UserRole.create adminUser, adminRole
+                        }
+			
+                        Person person = new Person(
+				firstName: 'sean',
+				lastName: 'cahill',
+				addressLine1: '53 Eaglewood',
+				addressLine2: 'Rochestown Ave.',
+				town: 'Dun Laoghaire',
+				county: 'Dublin',
+				country: 'Ireland',
+				dob: Date.parse("dd/MM/yyyy","22/09/1964"),
+                                telNo: '0879818357',
+				email: 'sean@nmm.ie'
+				)
+                        println person.firstName
+                        
+                        if (person.hasErrors()) {
+                            println person.errors
+                      
+                        }
+			person.save(flush: true)
         } 
     }
     def destroy = {
